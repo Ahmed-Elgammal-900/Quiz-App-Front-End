6deb313b4df776373b1df3e8d177a63f52e99d6f
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-08-21 07:47:39 +0300

    fix: correct char places for ranks

--- components/ui/leaderboard/organisms/Top3.tsx
+++ components/ui/leaderboard/organisms/Top3.tsx
@@ -3,13 +3,22 @@ import UserChar from "../molecules/UserChar"
 
 export default async function Top3() {
   const top3 = await getTop3()
-  if(!top3) throw new Error("not found top3")
+  if (!top3) throw new Error("not found top3")
   return (
     <section className="mt-15 flex items-end justify-center gap-x-5 md:px-20 lg:px-30">
-      {top3.length === 2 ? <div className="flex-1" /> : null}
-      {top3.map(({ userId, name, totalScore }, i) => (
-        <UserChar key={userId} name={name} score={totalScore} rank={i + 1} />
-      ))}
+      {top3.length === 2 ? (
+        <div className="flex-1" style={{ order: 1 }} />
+      ) : null}
+      {top3.map(({ userId, name, totalScore }, i) => {
+        const rank = i + 1
+        const order = rank === 1 ? 2 : rank === 2 ? 3 : 1
+
+        return (
+          <div key={userId} style={{ order }} className="flex">
+            <UserChar name={name} score={totalScore} rank={rank} />
+          </div>
+        )
+      })}
     </section>
   )
 }
