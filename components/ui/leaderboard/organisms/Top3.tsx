8f60760945c1249dd1b47f50f1a4abd45fd2cce9
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-08-21 07:58:16 +0300

    fix: optimize visual design with correct order for ranks

--- components/ui/leaderboard/organisms/Top3.tsx
+++ components/ui/leaderboard/organisms/Top3.tsx
@@ -1,24 +1,21 @@
 import { getTop3 } from "@/services/leaderboard.service"
 import UserChar from "../molecules/UserChar"
+import { getPodiumOrder } from "@/utils/component-util"
 
 export default async function Top3() {
   const top3 = await getTop3()
   if (!top3) throw new Error("not found top3")
   return (
     <section className="mt-15 flex items-end justify-center gap-x-5 md:px-20 lg:px-30">
-      {top3.length === 2 ? (
-        <div className="flex-1" style={{ order: 1 }} />
-      ) : null}
-      {top3.map(({ userId, name, totalScore }, i) => {
-        const rank = i + 1
-        const order = rank === 1 ? 2 : rank === 2 ? 3 : 1
-
-        return (
-          <div key={userId} style={{ order }} className="flex">
-            <UserChar name={name} score={totalScore} rank={rank} />
-          </div>
-        )
-      })}
+      {top3.length === 2 ? <div className="flex-1" /> : null}
+      {getPodiumOrder(top3).map((user) => (
+        <UserChar
+          key={user.userId}
+          name={user.name}
+          score={user.totalScore}
+          rank={user.rank}
+        />
+      ))}
     </section>
   )
 }
